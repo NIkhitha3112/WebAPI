713abaa67306a4b1a8de09c158191fd187c67aef
NIkhitha3112 <71930317+NIkhitha3112@users.noreply.github.com>
2026-02-18 10:44:56 -0500

    Refactor DepartmentController for cleaner code

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-using WebAPI.Models;
+
 
 namespace WebAPI.Controllers
 {
@@ -115,5 +106,6 @@ namespace WebAPI.Controllers
         }
     }
 }
+
 
 
